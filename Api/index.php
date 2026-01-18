7a3fa5fcdd60951775af12eb5b6019a106153ffd
alex01 <alextobar013@gmail.com>
2026-01-18 21:53:09 +0100

    vercel cambios
    vercel

---
+++ Api/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Carga el autoload de Composer (necesario para Laravel)
+require __DIR__ . '/../vendor/autoload.php';
+
+require __DIR__ . '/../public/index.php';
+
+// Opcional: fuerza entorno production
+if (!isset($_SERVER['APP_ENV'])) {
+    $_SERVER['APP_ENV'] = 'production';
+}
+
+// Redirige todo el tráfico al entry point real de Laravel
+require __DIR__ . '/../public/index.php';
